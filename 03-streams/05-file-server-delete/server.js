1dbdb4da8a832d99c15e8753f5b9f875cd0700b0
Artsiom Shabanau <artsiomshabanau@Artsioms-MacBook-Pro.local>
2023-10-17 15:34:00 +0200

    Add solution for the task-5 in module-3

--- 03-streams/05-file-server-delete/server.js
+++ 03-streams/05-file-server-delete/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -12,6 +13,26 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'DELETE':
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end("You can't request nested files");
+
+        return;
+      }
+
+      fs.unlink(filepath, (error) => {
+        if (!error) {
+          res.end('File deleted successfully');
+        } else {
+          if (error.code === 'ENOENT') {
+            res.statusCode = 404;
+            res.end('File is not found');
+          } else {
+            res.statusCode = 500;
+            res.end('Something went wrong');
+          }
+        }
+      });
 
       break;
 
