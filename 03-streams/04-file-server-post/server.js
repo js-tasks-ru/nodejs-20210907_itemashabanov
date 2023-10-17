160dfa54c60e7ce123a766b1a06d39933021d6a9
Artsiom Shabanau <artsiomshabanau@Artsioms-MacBook-Pro.local>
2023-10-17 15:33:53 +0200

    Add solution for the task-4 in module-3

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -1,8 +1,25 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
+
+const endConnection = (response, statusCode, message) => {
+  response.statusCode = statusCode;
+  response.end(message);
+};
+
+const destroyStream = (streamArray) => {
+  streamArray.forEach((stream) => stream.destroy());
+};
+
+const deleteFile = (fileStream, filepathArray) => {
+  filepathArray.forEach((filepath) => {
+    fileStream.unlink(filepath, (error) => {});
+  });
+};
 
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
@@ -12,12 +29,56 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'POST':
+      if (pathname.includes('/')) {
+        endConnection(res, 400, "You can't request nested files");
+
+        return;
+      }
+
+      const limitedStream = new LimitSizeStream({
+        limit: 1000000,
+        encoding: 'utf-8',
+      });
+      const writeStream = fs.createWriteStream(filepath, {
+        flags: 'wx',
+      });
+
+      req.pipe(limitedStream).pipe(writeStream);
+
+      writeStream.on('finish', () => {
+        endConnection(res, 201, 'File was successfully uploaded');
+      });
+
+      limitedStream.on('error', (error) => {
+        if (error.code === 'LIMIT_EXCEEDED') {
+          endConnection(res, 413, 'File size exceeded');
+        } else {
+          endConnection(res, 500, 'Something went wrong');
+        }
+
+        destroyStream([writeStream]);
+        deleteFile(fs, [filepath]);
+      });
+
+      writeStream.on('error', (error) => {
+        if (error.code === 'EEXIST') {
+          endConnection(res, 409, 'File already exists');
+
+          return;
+        }
+
+        endConnection(res, 500, 'Something went wrong');
+      });
+
+      req.on('aborted', () => {
+        destroyStream([writeStream, limitedStream]);
+        deleteFile(fs, [filepath]);
+      });
 
       break;
 
     default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+      endConnection(res, 501, 'Not implemented');
   }
 });
 
